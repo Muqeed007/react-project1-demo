1e721d311b0bab9137f39fa2c7e03fb82ed02cf8
Muqeed007 <ahmedalmuqeed2002@gmail.com>
2024-04-01 01:18:35 +0600

    updated Readingtime

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -5,7 +5,7 @@ const Bookmarks = ({bookmarks,readingTime}) => {
     return (
         <div className="md:w-1/3 text-center bg-amber-50">
             <div>
-                <h3 className='text-2xl text-center mt-4'>Reading Time:{readingTime}</h3>
+                <h3 className='text-2xl text-center mt-4 underline'>Reading Time:{readingTime}</h3>
             </div>
             <h2 className="text-3xl mt-4">Bookmarks:{bookmarks.length}</h2>
             {
